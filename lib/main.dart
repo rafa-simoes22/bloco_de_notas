1efa65a95a1c4d32b7eef9a8efa87ebd4e299337
Rafaela Simões <rafaelasimoes2203@gmail.com>
2023-09-06 14:06:49 -0300

    quase lá

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,8 @@ class NotesScreen extends StatefulWidget {
 class _NotesScreenState extends State<NotesScreen> {
   List<Map<String, dynamic>> notes = [];
   final TextEditingController _noteController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  bool _isAddingNote = false;
 
   @override
   void initState() {
@@ -54,17 +56,77 @@ class _NotesScreenState extends State<NotesScreen> {
   }
 
   void _addNote() {
-    if (_noteController.text.isNotEmpty) {
-      final newNote = {
-        'text': _noteController.text,
-        'timestamp': DateTime.now().toString(),
-      };
-      setState(() {
-        notes.add(newNote);
-        _noteController.clear();
-      });
-      _saveNotes();
-    }
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Adicionar Nota'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: 'Nome da Nota'),
+              ),
+              TextField(
+                controller: _noteController,
+                decoration: InputDecoration(labelText: 'Texto da Nota'),
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                if (_nameController.text.isNotEmpty &&
+                    _noteController.text.isNotEmpty) {
+                  final newNote = {
+                    'name': _nameController.text,
+                    'text': _noteController.text,
+                    'timestamp': DateTime.now().toString(),
+                  };
+                  setState(() {
+                    notes.add(newNote);
+                    _nameController.clear();
+                    _noteController.clear();
+                  });
+                  _saveNotes();
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Salvar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildNoteList() {
+    return ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        final note = notes[index];
+        final noteName = note['name'] ?? 'Nome não definido';
+        return ListTile(
+          title: Text(noteName),
+          subtitle: Text(note['timestamp']),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NoteDetailScreen(note: note),
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 
   @override
@@ -73,40 +135,95 @@ class _NotesScreenState extends State<NotesScreen> {
       appBar: AppBar(
         title: Text('Bloco de Notas'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: notes.length,
-              itemBuilder: (context, index) {
-                final note = notes[index];
-                return ListTile(
-                  title: Text(note['text']),
-                  subtitle: Text(note['timestamp']),
-                );
-              },
-            ),
+      body: _isAddingNote ? _buildNoteDetailScreen() : _buildNoteList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _isAddingNote = true;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _buildNoteDetailScreen() {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _nameController,
+            decoration: InputDecoration(labelText: 'Nome da Nota'),
           ),
-          Padding(
+        ),
+        Expanded(
+          child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _noteController,
-                    decoration: InputDecoration(
-                      hintText: 'Digite sua nota...',
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.add),
-                  onPressed: _addNote,
-                ),
-              ],
+            child: TextField(
+              controller: _noteController,
+              maxLines: null,
+              expands: true,
+              keyboardType: TextInputType.multiline,
+              decoration: InputDecoration(
+                labelText: 'Texto da Nota',
+                alignLabelWithHint: true,
+              ),
             ),
           ),
-        ],
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (_nameController.text.isNotEmpty &&
+                _noteController.text.isNotEmpty) {
+              final newNote = {
+                'name': _nameController.text,
+                'text': _noteController.text,
+                'timestamp': DateTime.now().toString(),
+              };
+              setState(() {
+                notes.add(newNote);
+                _nameController.clear();
+                _noteController.clear();
+                _isAddingNote = false;
+              });
+              _saveNotes();
+            }
+          },
+          child: Text('Salvar Nota'),
+        ),
+      ],
+    );
+  }
+}
+
+class NoteDetailScreen extends StatelessWidget {
+  final Map<String, dynamic> note;
+
+  NoteDetailScreen({required this.note});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(note['name'] ?? 'Nome não definido'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Data de Criação: ${note['timestamp']}',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            Text(
+              note['text'],
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
       ),
     );
   }
